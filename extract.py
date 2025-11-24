b6ef7c09f49d65ef1607fad71ee344278b050bce
1G4S <igor.sarnowski.21@gmail.com>
2025-11-24 19:00:22 +0100

    feat(extract): add normalize csv to dataframe and rename columns function to prepare data to be saved to json file

--- extract.py
+++ extract.py
@@ -2,12 +2,37 @@ import pandas as pd
 import os
 
 
+def normalize_csv_to_dataframe_and_rename_columns(file_path: str) -> pd.DataFrame:
+    if not isinstance(file_path, str):
+        raise TypeError(
+            f'Incorrect type of argument: {type(file_path)}'
+        )
+
+    if file_path.strip() == "":
+        raise ValueError(
+            f'Incorrect value of file path: {file_path}'
+        )
+
+    data = pd.read_csv(file_path)
+
+    ready_data = data.rename(columns={"created_at": "timestamp",
+                                      "field1": "PM2.5",
+                                      "field2": "PM10",
+                                      "field3": "temperature",
+                                      "field4": "humidity",
+                                      "field5": "PM2.5 norm",
+                                      "field6": "PM10 norm",
+                                      "field7": "city"})
+
+    return ready_data
+
+
 def save_data_to_json_file(data: pd.DataFrame, filename: str) -> None:
     """
-        Function that saves given data to json file in raw_data directory.
+    Function that saves given data to json file in raw_data directory.
 
     :param data: pd.Dataframe, data to be saved to file
-    :param filename: Name of the file with .json ending, that will be created if not exists, or will be overwrite
+    :param filename: Name of the file with .json ending, that will be created if not exists, or will be overwritten
                     if exists.
     :return: None
     """
@@ -20,8 +45,8 @@ def save_data_to_json_file(data: pd.DataFrame, filename: str) -> None:
             f'Incorrect value of arguments. Filename: "{filename}"')
 
     try:
-        if not os.path.exists('./raw_data'):
-            os.makedirs('./raw_data')
+        if not os.path.exists('./data'):
+            os.makedirs('./data')
 
     except PermissionError as e:
         raise PermissionError(
@@ -33,7 +58,7 @@ def save_data_to_json_file(data: pd.DataFrame, filename: str) -> None:
             f'Cannot create a directory.'
         ) from e
 
-    file_path = './raw_data/' + filename
+    file_path = './data/' + filename
     json_data = data.to_json(orient='records', indent=4)
 
     try:
